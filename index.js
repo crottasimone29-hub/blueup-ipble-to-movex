69584d4deefb3aced39c29eff59a92195388506a
Assistenza BCare <assitenza@awswitch.com>
2026-05-11 10:01:00 +0200

    Data in now sent to Movex

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const https = require("https");
 
 const PORT = 3099;
 
@@ -19,6 +20,51 @@ function formatDataForMovex(data) {
     };
 }
 
+function sendToMovex(beaconData) {
+    const movexApiUrl = "https://movex.awswitch.com/api/v01/pdr";
+    const requestPayload = JSON.stringify(beaconData);
+    
+    const httpsOptions = {
+        hostname: "movex.awswitch.com",
+        path: "/api/v01/collect",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Content-Length": Buffer.byteLength(requestPayload),
+            "from": "BCareEVAQ",
+        },
+    };
+    
+    const httpsRequest = https.request(httpsOptions, (serverResponse) => {
+        console.log(`\n=== MovEx Response ===`);
+        console.log(`Status Code: ${serverResponse.statusCode}`);
+        console.log(`Headers:`, serverResponse.headers);
+        
+        let responseBody = "";
+        
+        serverResponse.on("data", (chunk) => {
+            responseBody += chunk.toString();
+        });
+        
+        serverResponse.on("end", () => {
+            try {
+                const parsedResponse = JSON.parse(responseBody);
+                console.log(`Response Body:`, JSON.stringify(parsedResponse, null, 2));
+            } catch (error) {
+                console.log(`Response Body:`, responseBody);
+            }
+            console.log(`======================\n`);
+        });
+    });
+    
+    httpsRequest.on("error", (error) => {
+        console.error("Error sending data to MovEx:", error);
+    });
+    
+    httpsRequest.write(requestPayload);
+    httpsRequest.end();
+}
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -32,6 +78,8 @@ app.post("/ibeacons", (req, res) => {
 
     const formatted = formatDataForMovex(req.body);
     console.log("Formatted:", JSON.stringify(formatted, null, 2));
+
+    sendToMovex(formatted);
 
     res.status(200).json({ status: "ok" });
 });
